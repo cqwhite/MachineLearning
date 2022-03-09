cd1120671f8f552ca6657a29c418d518d531e17a
David Chalifoux <dccfoux@gmail.com>
2022-03-08 21:55:15 -0500

    Training

--- E08/E08.py
+++ E08/E08.py
@@ -1,5 +1,130 @@
 # David Chalifoux & Connor White
 from math import exp
+import random
+
+# Input nodes
+i1Node = {
+    "value": 1,
+    "weight_h1": random.uniform(-.5, .5),
+    "weight_h2": random.uniform(-.5, .5)
+}
+i2Node = {
+    "value": 1,
+    "weight_h1": random.uniform(-.5, .5),
+    "weight_h2": random.uniform(-.5, .5)
+}
+
+# Hidden nodes
+h1Node = {
+    "value": random.uniform(-.5, .5),
+    "weight_o1": random.uniform(-.5, .5),
+    "weight_o2": random.uniform(-.5, .5)
+}
+h2Node = {
+    "value": random.uniform(-.5, .5),
+    "weight_o1": random.uniform(-.5, .5),
+    "weight_o2": random.uniform(-.5, .5)
+}
+
+# Output nodes
+o1Node = {
+    "value": random.uniform(-.5, .5),
+    "true": 0
+}
+o2Node = {
+    "value": random.uniform(-.5, .5),
+    "true": 1
+}
+
+
+def feedForward():
+    # Calc h1
+    temp = i1Node["weight_h1"] + i2Node["weight_h1"] + h1Node["value"]
+    h1Node["feedForward"] = 1 / (1+exp(temp * -1))
+
+    # Calc h2
+    temp = i1Node["weight_h2"] + i2Node["weight_h2"] + h2Node["value"]
+    h2Node["feedForward"] = 1 / (1+exp(temp * -1))
+
+    # Calc o1
+    temp = (h1Node["weight_o1"] * h1Node["feedForward"]) + \
+        (h2Node["weight_o1"] * h2Node["feedForward"]) + o1Node["value"]
+    o1Node["feedForward"] = 1 / (1+exp(temp * -1))
+    # Calc o2
+    temp = (h1Node["weight_o2"] * h1Node["feedForward"]) + \
+        (h2Node["weight_o2"] * h2Node["feedForward"]) + o2Node["value"]
+    o2Node["feedForward"] = 1 / (1+exp(temp * -1))
+
+
+def calcErr():
+    # Calc err of o1
+    temp = o1Node["feedForward"] * \
+        (1-o1Node["feedForward"])*(o1Node["true"]-o1Node["feedForward"])
+    o1Node["error"] = temp
+    # Calc err of o2
+    temp = o2Node["feedForward"] * \
+        (1-o2Node["feedForward"])*(o2Node["true"]-o2Node["feedForward"])
+    o2Node["error"] = temp
+    # Calc err of h1
+    temp = h1Node["feedForward"] * (1 - h1Node["feedForward"]) * (
+        (o1Node["error"] * h1Node["weight_o1"]) + (o2Node["error"] * h1Node["weight_o2"]))
+    h1Node["error"] = temp
+    # Calc err of h2
+    temp = h2Node["feedForward"] * (1 - h2Node["feedForward"]) * (
+        (o1Node["error"] * h2Node["weight_o1"]) + (o2Node["error"] * h2Node["weight_o2"]))
+    h2Node["error"] = temp
+
+
+def backPropagation():
+    # Calc weight and bias adjustment of o1
+    o1Node["value"] = o1Node["value"] + o1Node["error"]
+    # Calc weight and bias adjustment of o2
+    o2Node["value"] = o2Node["value"] + o2Node["error"]
+    # Calc weight and bias adjustment of h1
+    h1Node["weight_o1"] = h1Node["weight_o1"] + \
+        o1Node["error"] * h1Node["feedForward"]
+    h1Node["weight_o2"] = h1Node["weight_o2"] + \
+        o2Node["error"] * h1Node["feedForward"]
+    h1Node["value"] = h1Node["value"] + h1Node["error"]
+    # Calc weight and bias adjustment of h2
+    h2Node["weight_o1"] = h2Node["weight_o1"] + \
+        o1Node["error"] * h2Node["feedForward"]
+    h2Node["weight_o2"] = h2Node["weight_o2"] + \
+        o2Node["error"] * h2Node["feedForward"]
+    h2Node["value"] = h2Node["value"] + h2Node["error"]
+    # Calc weight and bias adjustment of i1
+    i1Node["weight_h1"] = i1Node["weight_h1"] + \
+        h1Node["error"] * i1Node["value"]
+    i1Node["weight_h2"] = i1Node["weight_h2"] + \
+        h2Node["error"] * i1Node["value"]
+    # Calc weight and bias adjustment of i2
+    i2Node["weight_h1"] = i2Node["weight_h1"] + \
+        h1Node["error"] * i2Node["value"]
+    i2Node["weight_h2"] = i2Node["weight_h2"] + \
+        h2Node["error"] * i2Node["value"]
+
+
+def train(trainingData):
+    for trainingSet in trainingData:
+        print("Training on", trainingSet)
+
+        # Setup nodes
+        i1Node["value"] = trainingSet[0]
+        i2Node["value"] = trainingSet[1]
+        o1Node["true"] = trainingSet[2]
+        o2Node["true"] = trainingSet[3]
+
+        # Run full back-propagation
+        for i in range(100):
+            feedForward()
+            calcErr()
+            backPropagation()
+
+        # Determine result
+        print(o1Node)
+        print(o2Node)
+        print("")
+
 
 # Training data
 # [input1, input1, output1, output2]
@@ -9,106 +134,4 @@ trainingData = [
     [0, 0, 0, 1],
     [0, 1, 1, 0]
 ]
-
-# Input nodes
-i1Node = {
-    "value": 1,
-    "weight_h1": .2,
-    "weight_h2": -.3
-}
-i2Node = {
-    "value": 1,
-    "weight_h1": .4,
-    "weight_h2": .3
-}
-
-# Hidden nodes
-h1Node = {
-    "value": .1,
-    "weight_o1": .3,
-    "weight_o2": -.2
-}
-h2Node = {
-    "value": -.1,
-    "weight_o1": .5,
-    "weight_o2": -.4
-}
-
-# Output nodes
-o1Node = {
-    "value": -.2,
-    "true": 0
-}
-o2Node = {
-    "value": .3,
-    "true": 1
-}
-
-
-def feedForward():
-    # Calc h1
-    temp = i1Node["weight_h1"] + i2Node["weight_h1"] + h1Node["value"]
-    h1Node["feedForward"] = round(1 / (1+exp(temp * -1)), 2)
-
-    # Calc h2
-    temp = i1Node["weight_h2"] + i2Node["weight_h2"] + h2Node["value"]
-    h2Node["feedForward"] = round(1 / (1+exp(temp * -1)), 2)
-
-    # Calc o1
-    temp = (h1Node["weight_o1"] * h1Node["feedForward"]) + \
-        (h2Node["weight_o1"] * h2Node["feedForward"]) + o1Node["value"]
-    o1Node["feedForward"] = round(1 / (1+exp(temp * -1)), 2)
-    # Calc o2
-    temp = (h1Node["weight_o2"] * h1Node["feedForward"]) + \
-        (h2Node["weight_o2"] * h2Node["feedForward"]) + o2Node["value"]
-    o2Node["feedForward"] = round(1 / (1+exp(temp * -1)), 2)
-
-
-def calcErr():
-    # Calc err of o1
-    temp = o1Node["feedForward"] * \
-        (1-o1Node["feedForward"])*(o1Node["true"]-o1Node["feedForward"])
-    o1Node["error"] = round(temp, 2)
-    # Calc err of o2
-    temp = o2Node["feedForward"] * \
-        (1-o2Node["feedForward"])*(o2Node["true"]-o2Node["feedForward"])
-    o2Node["error"] = round(temp, 2)
-    # Calc err of h1
-    temp = h1Node["feedForward"] * (1 - h1Node["feedForward"]) * (
-        (o1Node["error"] * h1Node["weight_o1"]) + (o2Node["error"] * h1Node["weight_o2"]))
-    h1Node["error"] = round(temp, 3)
-
-    # Calc err of h2
-    temp = h2Node["feedForward"] * (1 - h2Node["feedForward"]) * (
-        (o1Node["error"] * h2Node["weight_o1"]) + (o2Node["error"] * h2Node["weight_o2"]))
-    h2Node["error"] = round(temp, 3)
-
-
-def backPropagation():
-    # calc weight and Bias Adjustment of o1
-    tempNode = o1Node["value"] + o1Node["error"]
-    print(tempNode)
-    o1ToH1 = h1Node["weight_o1"] + o1Node["error"] * h1Node["feedForward"]
-    o1ToH2 = h2Node["weight_o1"] + o1Node["error"] * h2Node["feedForward"]
-
-    # calc weight and Bias Adjustment of o2
-    tempNode = o2Node["value"] + o2Node["error"]
-    print(tempNode)
-    o2ToH1 = h1Node["weight_o1"] + o2Node["error"] * h1Node["feedForward"]
-    o2ToH2 = h2Node["weight_o1"] + o2Node["error"] * h2Node["feedForward"]
-
-    # calc weight and Bias Adjustment of h1
-    tempNode = h1Node["value"] + h1Node["error"]
-    print(tempNode)
-    h1ToI1 = i1Node["weight_h1"] + h1Node["error"] * 1
-    h1ToI2 = i2Node["weight_h1"] + h1Node["error"] * 1
-    # calc weight and Bias Adjustment of h2
-    tempNode = h2Node["value"] + h2Node["error"]
-    print(tempNode)
-    h2ToI1 = i1Node["weight_h2"] + h2Node["error"] * 1
-    h2ToI2 = i2Node["weight_h2"] + h2Node["error"] * 1
-
-
-feedForward()
-calcErr()
-backPropagation()
+train(trainingData)
