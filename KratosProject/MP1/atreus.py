b72ac9547c2662414fdf754c54e8517ef5b65bf4
Connor White <cqwhite19@gmail.com>
2022-04-23 22:10:20 -0400

    added descriptions

--- KratosProject/MP1/atreus.py
+++ KratosProject/MP1/atreus.py
@@ -1,3 +1,14 @@
+"""
+Example of using a nuerel network for detecting an anomoly (true or false (0, 1))
+
+RESULTS:
+    50 Epochs:
+    loss: 0.5907 - accuracy: 0.7227
+    Train Accuracy: 0.723
+    Test Accuracy: 0.746
+
+David Chalifoux, Connor White, Quinn Partain, Micah Odell
+"""
 from numpy import argmax
 from pandas import read_csv
 from sklearn.model_selection import train_test_split
@@ -8,7 +19,7 @@ from tensorflow.keras.layers import Flatten
 import numpy as np
 
 # load the dataset
-fileName = "./KratosProject/MP1_connor_quinn/sat41589.txt"
+fileName = "./KratosProject/MP1/sat41589.txt"
 inFile = open(fileName, 'r')
 df = read_csv(inFile, header=None)
 # split into input and output columns
@@ -33,7 +44,7 @@ model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
 # fit the model
-model.fit(X_train, y_train, epochs=500, verbose=2)
+model.fit(X_train, y_train, epochs=50, verbose=2)
 
 # evaluate the model
 loss, acc = model.evaluate(X_train, y_train, verbose=0)
@@ -42,7 +53,7 @@ loss, acc = model.evaluate(X_test, y_test, verbose=0)
 print('Test Accuracy: %.3f' % acc)
 
 # make a prediction with a new example
-fileName = "./KratosProject/MP1_connor_quinn/sat42709.txt"
+fileName = "./KratosProject/MP1/sat42709.txt"
 inFileTester = open(fileName, 'r')
 tf = read_csv(inFileTester, header=None)
 testRows = tf.values[:, 2:-1]
@@ -50,6 +61,5 @@ testRows = tf.values[:, 2:-1]
 row = testRows[1]
 row = np.asarray(row).astype('float32')
 
-print(row)
 yhat = model.predict([[row]])
 print('Predicted: %s (class=%d)' % (yhat, argmax(yhat)))
