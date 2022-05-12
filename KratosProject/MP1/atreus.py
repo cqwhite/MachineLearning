0d150045f4563928dd01ac5841ee5671ddd918c3
Connor White <cqwhite19@gmail.com>
2022-05-12 11:18:22 -0400

    testing stuff

--- KratosProject/MP1/atreus.py
+++ KratosProject/MP1/atreus.py
@@ -18,48 +18,83 @@ from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Flatten
 import numpy as np
 
-# load the dataset
-fileName = "./KratosProject/MP1/sat41589.txt"
-inFile = open(fileName, 'r')
-df = read_csv(inFile, header=None)
-# split into input and output columns
-X, y = df.values[:, 2:-1], df.values[:, -1]
-X = np.asarray(X).astype('float32')
-y = np.asarray(y).astype('float32')
-# split into train and test datasets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
-print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
 
-# determine the number of input features
-n_features = X_train.shape[1]
-print("n=", n_features)
+def MLP(activationOne, activationTwo, nodeCountOne, NodeCountTwo, testSize, epochsNumber):
+    # load the dataset
+    fileName = "./KratosProject/MP1/sat41589.txt"
+    inFile = open(fileName, 'r')
+    df = read_csv(inFile, header=None)
+    # split into input and output columns
+    X, y = df.values[:, 2:-1], df.values[:, -1]
+    X = np.asarray(X).astype('float32')
+    y = np.asarray(y).astype('float32')
+    # split into train and test datasets
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=testSize)
+    print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
 
-# define model
-model = Sequential()
-model.add(Dense(6, activation="sigmoid", input_shape=(n_features,)))
-model.add(Dense(12, activation='softmax'))
+    # determine the number of input features
+    n_features = X_train.shape[1]
+    print("n=", n_features)
 
-# compile the model
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    # define model
+    model = Sequential()
+    model.add(Dense(nodeCountOne, activation=activationOne,
+              input_shape=(n_features,)))
+    model.add(Dense(NodeCountTwo, activation=activationTwo))
 
-# fit the model
-model.fit(X_train, y_train, epochs=50, verbose=2)
+    # compile the model
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-# evaluate the model
-loss, acc = model.evaluate(X_train, y_train, verbose=0)
-print('Train Accuracy: %.3f' % acc)
-loss, acc = model.evaluate(X_test, y_test, verbose=0)
-print('Test Accuracy: %.3f' % acc)
+    # fit the model
+    model.fit(X_train, y_train, epochs=epochsNumber, verbose=2)
 
-# make a prediction with a new example
-fileName = "./KratosProject/MP1/sat42709.txt"
-inFileTester = open(fileName, 'r')
-tf = read_csv(inFileTester, header=None)
-testRows = tf.values[:, 2:-1]
+    # evaluate the model
+    loss, accTrain = model.evaluate(X_train, y_train, verbose=0)
+    print('Train Accuracy: %.3f' % accTrain)
+    loss, accTest = model.evaluate(X_test, y_test, verbose=0)
+    print('Test Accuracy: %.3f' % accTest)
 
-row = testRows[1]
-row = np.asarray(row).astype('float32')
+    # make a prediction with a new example
+    fileName = "./KratosProject/MP1/sat42709.txt"
+    inFileTester = open(fileName, 'r')
+    tf = read_csv(inFileTester, header=None)
+    testRows = tf.values[:, 2:-1]
 
-yhat = model.predict([[row]])
-print('Predicted: %s (class=%d)' % (yhat, argmax(yhat)))
+    row = testRows[1]
+    row = np.asarray(row).astype('float32')
+
+    yhat = model.predict([[row]])
+    #print('Predicted: %s (class=%d)' % (yhat, argmax(yhat)))
+    print("Train:", accTrain, "Test", accTest)
+    accuracyTotal = (accTrain + accTest)/2
+    return accuracyTotal
+
+
+activationList = ['sigmoid', 'softmax']
+nodeCountList = [6, 12, 24, 48, 72]
+testSizeList = [.33, .25, .1]
+
+# Best combination
+MLP('sigmoid', 'softmax', 6, 24, 0.1, 50)
+
+# Find best combo
+bestAcc = -1
+
+'''
+for firstActivation in activationList:
+    for secondActivation in activationList:
+        for firstNode in nodeCountList:
+            for secondNode in nodeCountList:
+                for testSize in testSizeList:
+                    currentAccuracy = MLP(
+                        firstActivation, secondActivation, firstNode, secondNode, testSize, 10)
+                    if (currentAccuracy > bestAcc):
+                        bestAcc = currentAccuracy
+                        bestAccList = [
+                            firstActivation, secondActivation, firstNode, secondNode, testSize]
+
+print(bestAccList)
+print(bestAcc)
+'''
--- KratosProject/MP2/connormain.py
+++ KratosProject/MP2/connormain.py
@@ -1,3 +1,10 @@
+# TO FINISH THE PROJECT
+# TEST WITH LINE 7 OUT
+# DO paramater tests of different hidden units and different time steps
+# make a count and print it
+# try the psa file
+
+
 import matplotlib.pyplot as plt
 import math
 from sklearn.metrics import mean_squared_error
@@ -11,10 +18,12 @@ from pandas import read_csv
 
 
 def get_train_test(textFile, split_percent=0.8):
-    df = read_csv(textFile, usecols=[1], engine='python')
+    df = read_csv(textFile, usecols=[1, 2, 3, 4, 5, 6, 7], engine='python')
     data = np.array(df.values.astype('float32'))
+    #print("DAAAATTTAAAA", data[0])
     scaler = MinMaxScaler(feature_range=(0, 1))
     data = scaler.fit_transform(data).flatten()
+
     n = len(data)
     # Point for splitting data into train and test
     split = int(n*split_percent)
@@ -31,6 +40,8 @@ def get_XY(dat, time_steps):
     rows_x = len(Y)
     X = dat[range(time_steps*rows_x)]
     X = np.reshape(X, (rows_x, time_steps, 1))
+   # print('THIS IS THE Xs', X)
+    # print("THIS IS THE Ys", Y)
     return X, Y
 
 
@@ -61,33 +72,78 @@ def plot_result(trainY, testY, train_predict, test_predict):
     plt.figure(figsize=(15, 6), dpi=80)
     plt.plot(range(rows), actual)
     plt.plot(range(rows), predictions)
-    plt.axvline(x=len(trainY), color='r')
+    #plt.axvline(x=len(trainY), color='r')
     plt.legend(['Actual', 'Predictions'])
     plt.xlabel('Observation number after given time steps')
     plt.ylabel('Sunspots scaled')
     plt.title(
         'Actual and Predicted Values. The Red Line Separates The Training And Test Examples')
+    plt.show()
 
 
-trainFile = "./KratosProject/MP2/sat41589.txt"
-sunspots_url = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/monthly-sunspots.csv'
-time_steps = 12
-train_data, test_data, data = get_train_test(sunspots_url)
-trainX, trainY = get_XY(train_data, time_steps)
-testX, testY = get_XY(test_data, time_steps)
+def rnn(timeStep, timeStepUnit, hiddenUnit, denseUnit):
+    trainFile = "./KratosProject/MP2/sat42709.txt"
+    sunspots_url = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/monthly-sunspots.csv'
+    time_steps = timeStep
+    train_data, test_data, data = get_train_test(trainFile)
+    trainX, trainY = get_XY(train_data, time_steps)
+    testX, testY = get_XY(test_data, time_steps)
 
-# Create model and train
-model = create_RNN(hidden_units=3, dense_units=1, input_shape=(time_steps, 1),
-                   activation=['tanh', 'tanh'])
-                   
-model.fit(trainX, trainY, epochs=20, batch_size=1, verbose=2)
+    # Create model and train
+    model = create_RNN(hidden_units=hiddenUnit, dense_units=denseUnit, input_shape=(time_steps, timeStepUnit),
+                       activation=['tanh', 'tanh'])
 
-# make predictions
-train_predict = model.predict(trainX)
-test_predict = model.predict(testX)
+    # Change Verbose to 2
+    model.fit(trainX, trainY, epochs=2, batch_size=1, verbose=2)
 
-# Print error
-print_error(trainY, testY, train_predict, test_predict)
+    # make predictions
+    train_predict = model.predict(trainX)
+    test_predict = model.predict(testX)
 
-# Plot result
-plot_result(trainY, testY, train_predict, test_predict)
+    # Print error
+    print_error(trainY, testY, train_predict, test_predict)
+
+    return train_predict,test_predict
+
+    # Plot result
+   # plot_result(trainY, testY, train_predict, test_predict)
+
+
+# Best Run
+#rnn(12, 1, 3, 1)
+f = open("rnnTestResults.txt", "a")
+
+
+timeStepList = [12, 6, 3, 1]
+timeStepUnitList = [1, .1, .01]
+hiddenUnitList = [3, 6, 1]
+denseUnitList = [1, 2, 3]
+bestRMSE = -1
+bestRMSEList = []
+count = 0
+for timeStep in timeStepList:
+    for timeStepUnit in timeStepUnitList:
+        for hiddenUnit in hiddenUnitList:
+            for denseUnit in denseUnitList:
+                print("STARTING:",
+                      timeStep, timeStepUnit, hiddenUnit, denseUnit)
+                count += 1
+                currentRMSE = rnn(timeStep, timeStepUnit,
+                                  hiddenUnit, denseUnit)
+                if (currentRMSE > bestRMSE):
+                    bestRMSE = currentRMSE
+                    bestRMSEList = [timeStep, timeStepUnit,
+                                    hiddenUnit, denseUnit]
+                print(count)
+                f.write("Test Round", count)
+                f.write("Test List", timeStep, timeStepUnit,
+                        hiddenUnit, denseUnit)
+                f.write("Current RMSE", currentRMSE)
+                f.write("Best List", bestRMSEList)
+                f.write("Best RMSE", bestRMSE)
+                f.write("-----------------------------------")
+               
+
+print("Best RSMELIST", bestRMSEList)
+print("RSME:", bestRMSE)
+f.close()
